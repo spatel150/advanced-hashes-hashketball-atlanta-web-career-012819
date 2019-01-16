65f8ee8df3356812c413edad0be39d6e9101347f
Samidh Patel <spatel13456@gmail.com>
2019-01-16 20:42:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,11 +121,9 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
-    binding.pry
     team_data.each do |attribute, data|
       if attribute == :players
         data.each do |player, stats|
-          binding.pry
           if player == player_name
           end
         end
