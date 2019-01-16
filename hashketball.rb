b176e26ccd346b79e75a335825f24ef7dc6b9707
Samidh Patel <spatel13456@gmail.com>
2019-01-16 21:38:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,10 @@ end
 def player_stats(player_name)
   game_hash.each do |locate, team_data|
       team_data.each do |key, value|
-        
+        if key == :players
+          value.each do |player, stats|
+            if player = player_name
+              return stats[:players]
 
 
 
